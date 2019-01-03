e1d05bf043546f765448d575f2e076a4fd598bba
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-01-03 22:12:11 +0100

    easter egg

--- apps/pwirProj/src/pwirProj_root.erl
+++ apps/pwirProj/src/pwirProj_root.erl
@@ -5,5 +5,5 @@
 init(Req0, Opts) ->
 	Req = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/plain">>
-	}, <<"Hello world!">>, Req0),
+	}, <<"O nie! ALARM! OPUSZCZAĆ ROLETY!">>, Req0),
 	{ok, Req, Opts}.
