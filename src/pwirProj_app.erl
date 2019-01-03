f0aa69e5ac55852c7a7657f9b01055d40f12af8e
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-01-03 21:41:51 +0100

    add an cowboy enpoint code

--- src/pwirProj_app.erl
+++ src/pwirProj_app.erl
@@ -15,7 +15,17 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    pwirProj_sup:start_link().
+    {ok, Pid} = pwirProj_sup:start_link(),
+    Routes = [{
+        {"/", pwirProj_root, []}
+    }],
+    Dispatch = cowboy_router:compile(Routes),
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 2938} ],
+    ProtoOpts = #{env => #{dispatch => Dispatch}},
+    {ok, _} = cowboy:start_clear(chicken_poo_poo,
+        TransOpts, ProtoOpts),
+
+    {ok, Pid}.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
